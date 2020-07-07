d11dd0f7d1f3f9cd2bb1c85ca0b6ef8a9a835964
Iury Castro <Iury.Castro@adp.com>
2020-07-06 21:25:17 -0300

    Adding Spring Boot Files

---
+++ src/main/java/com/demoapp/votingpoll/VotingPollApplication.java
@@ -0,0 +1,13 @@
+package com.demoapp.votingpoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingPollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingPollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoapp/votingpoll/VotingPollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoapp.votingpoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingPollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
